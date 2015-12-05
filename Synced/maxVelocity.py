29a5cc1cc1a2bf5313d935893323db15d893b4fa
JDTyvand <jorgentyvand@gmail.com>
2015-12-05 23:13:49 +0100

    Updated files.

--- Synced/maxVelocity.py
+++ Synced/maxVelocity.py
@@ -1,27 +1,28 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 names = ['184_03_default','184_03_linearUpwind','184_03_linearUpwind_convection','184_03_QUICK', \
 	 '184_03_QUICK_convection','184_03_SFCD','184_03_SFCD_convection']
-
+max_vals = []
+max_times = []
 for i in range(len(names)):
 	file = open(names[i])
-	f = open(str(names[i] + '.txt'), 'a')
 	for j in range(78):
-		line = file.readline()
-
+		file.readline()
+	max_u = 0
+	max_time = 0
 	for line in file.readlines():
 		string = line.replace('(', '')
 		string = string.replace(')', '')
 		time = float(string.split()[0])
 		vals = string.split()[1:]
 		vals = np.array(map(float, vals))
-		u = []
-		for k in range(0,len(vals),3):
-			u.append(vals[k])
-		if time == 92:
-			for l in range(len(u)):
-				f.write(str(u[l]) + '\n')
-		
+		if vals.max() > max_u:
+			max_u = vals.max()
+			max_time = time
+	max_vals.append(max_u)
+	max_times.append(max_time)
 	file.close()
-	f.close()
+print max_vals
+print np.mean(max_times)
 
