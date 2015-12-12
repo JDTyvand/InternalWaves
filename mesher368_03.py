cc7aac79ce7b49967d3ff3f6247a7d2a3141d09d
JDTyvand <jorgentyvand@gmail.com>
2015-12-12 16:29:38 +0100

    Updated files.

---
+++ mesher368_03.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+f = open('mesher368_03.txt', 'a')
+lscale = np.linspace(5, 72.95, 29)
+rscale = np.linspace(5, 72.95, 39)
+print lscale
+print rscale
+lcount = 1
+rcount = 1
+for i in range(144):
+	print ('Line %d' % (i+1))
+	if i < 42:
+		print('Uniform lower temperature 5')
+		for j in range(368):
+			f.write(str(5))
+			f.write('\n')
+	elif 41 < i < 69:
+		print lscale[lcount]
+		print('Left stratification, 5 right')
+		for j in range(10):
+			f.write(str(lscale[lcount]))
+			f.write('\n')
+		lcount += 1
+		for j in range(358):
+			f.write(str(5))
+			f.write('\n')
+	elif 68 < i < 106:
+		print('Fresh water left, 5 right')
+		for j in range(10):
+			f.write(str(72.95))
+			f.write('\n')
+		for j in range(358):
+			f.write(str(5))
+			f.write('\n')
+	else:
+		print rcount
+		print('Fresh water left, stratification right')
+		print rscale[rcount]
+		for j in range(10):
+			f.write(str(72.95))
+			f.write('\n')
+		for j in range(358):
+			f.write(str(rscale[rcount]))
+			f.write('\n')
+		rcount += 1
+
+f.close()
